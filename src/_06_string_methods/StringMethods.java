7dc01154b0aca4e26a72c0a7f5288bf5a5a6ae8c
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-03-27 13:28:29 -0700

    HappyPetIP

--- src/_06_string_methods/StringMethods.java
+++ src/_06_string_methods/StringMethods.java
@@ -1,31 +1,35 @@
 package _06_string_methods;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
-
-
 
 public class StringMethods {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String s = "silly";
 		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		
+		// HINT: .charAt
+		//System.out.println(s.charAt(2));
 		// 3. Print the length of your String to the console.
-		//    HINT: .length()
-
+		// HINT: .length()
+		//System.out.println(s.length());
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
-
-		
-		// 5. Pick a char inside your String, and use a loop to determine
-		//    what position/index in the String the char is located.
-		//    Print the char's position to the console.
-		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
-		
+		// HINT: use .length() to determine how many loops
+		for (int i = 0; i < s.length(); i++) {
+			System.out.print(s.charAt(i));
+		}
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == 'y') {
+				System.out.println(i);
+			}
 	}
+
+	// 5. Pick a char inside your String, and use a loop to determine
+	// what position/index in the String the char is located.
+	// Print the char's position to the console.
+	// EXAMPLE: if your string is "abc" and you are searching
+	// for char 'b', then print "b is at index 1"
+
 }
-
-
+}
